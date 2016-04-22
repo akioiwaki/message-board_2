0dc93fe37d9a3840828b458113253940d2c3a9fc
akioiwaki <akioiwaki@gmail.com>
2016-04-22 06:04:26 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ActiveRecord::Base
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
     # validates :age , presence: true 
-    validates :age , numericality:{ :greater_than => 0 } 
+    validates :age , numericality:{ :greater_than_or_equal_to => 0 } 
 end
 
   
